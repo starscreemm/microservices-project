6527505fb5609e2724185cefd9d156ab5e141e2b
starscreemm <startscreemm8@gmail.com>
2023-12-23 18:22:34 +0200

    "add persistence layer"

--- api/src/main/java/se/magnus/api/core/product/ProductService.java
+++ api/src/main/java/se/magnus/api/core/product/ProductService.java
@@ -1,12 +1,24 @@
 package se.magnus.api.core.product;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 public interface ProductService {
+
+  /**
+   * Sample usage, see below.
+   *
+   * curl -X POST $HOST:$PORT/product \
+   *   -H "Content-Type: application/json" --data \
+   *   '{"productId":123,"name":"product 123","weight":123}'
+   *
+   * @param body A JSON representation of the new product
+   * @return A JSON representation of the newly created product
+   */
+  @PostMapping(
+    value    = "/product",
+    consumes = "application/json",
+    produces = "application/json")
+  Product createProduct(@RequestBody Product body);
 
   /**
    * Sample usage: "curl $HOST:$PORT/product/1".
@@ -14,23 +26,16 @@ public interface ProductService {
    * @param productId Id of the product
    * @return the product, if found, else null
    */
-  @Operation(
-          summary =
-                  "${api.product-composite.get-composite-product.description}",
-          description =
-                  "${api.product-composite.get-composite-product.notes}")
-  @ApiResponses(value = {
-          @ApiResponse(responseCode = "200", description =
-                  "${api.responseCodes.ok.description}"),
-          @ApiResponse(responseCode = "400", description =
-                  "${api.responseCodes.badRequest.description}"),
-          @ApiResponse(responseCode = "404", description =
-                  "${api.responseCodes.notFound.description}"),
-          @ApiResponse(responseCode = "422", description =
-                  "${api.responseCodes.unprocessableEntity.description}")
-  })
   @GetMapping(
     value = "/product/{productId}",
     produces = "application/json")
   Product getProduct(@PathVariable int productId);
+
+  /**
+   * Sample usage: "curl -X DELETE $HOST:$PORT/product/1".
+   *
+   * @param productId Id of the product
+   */
+  @DeleteMapping(value = "/product/{productId}")
+  void deleteProduct(@PathVariable int productId);
 }
